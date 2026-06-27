38c9229d36f64051df99ce580d69c34e7473acce
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 19:55:55 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from routes.auth import router as auth_router
+from routes.users import router as user_router
+from routes.roles import router as role_router
+
+app = FastAPI(
+    title="x402-auth",
+    version="1.0.0"
+)
+
+app.include_router(auth_router)
+app.include_router(user_router)
+app.include_router(role_router)
+
+
+@app.get("/")
+def home():
+
+    return {
+        "service": "x402-auth",
+        "status": "running"
+    }

20efe8011bc585aff83a64f379d0445255ce20d5
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 19:56:20 +0100

    Create auth.py

---
+++ routes/auth.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post("/register")
+def register():
+
+    return {
+        "message": "User registered"
+    }
+
+
+@router.post("/login")
+def login():
+
+    return {
+        "message": "User logged in"
+    }
+
+
+@router.post("/logout")
+def logout():
+
+    return {
+        "message": "User logged out"
+    }
